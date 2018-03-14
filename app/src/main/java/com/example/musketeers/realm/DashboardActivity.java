891a0f25af3ab41e1db863b3f2174fb7bb2df7a6
RajPrabhakar <rajprabhakar1804@gmail.com>
2018-03-14 13:59:16 +0530

    Dashboard Linear Layout

--- app/src/main/java/com/example/musketeers/realm/DashboardActivity.java
+++ app/src/main/java/com/example/musketeers/realm/DashboardActivity.java
@@ -12,21 +12,5 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-
-        ArrayList<Word> appliances = new ArrayList<>();
-        appliances.add(new Word("Air Conditioner", false, R.drawable.ac));
-        appliances.add(new Word("Blender", false, R.drawable.blender));
-        appliances.add(new Word("Computer", false, R.drawable.computer));
-        appliances.add(new Word("Iron Box", false, R.drawable.iron));
-        appliances.add(new Word("Microwave Oven", false, R.drawable.microwave));
-        appliances.add(new Word("Modem", false, R.drawable.modem));
-        appliances.add(new Word("Refrigerator", false, R.drawable.refrigerator));
-        appliances.add(new Word("Television", false, R.drawable.tv));
-        appliances.add(new Word("Washing Machine", false, R.drawable.wash));
-
-        WordAdapter adapter = new WordAdapter(this, appliances);
-
-        ListView applianceListView = findViewById(R.id.list);
-        applianceListView.setAdapter(adapter);
     }
 }
