c57542919aa17da3ce1a1015714800fbd9641154
RajPrabhakar <rajprabhakar1804@gmail.com>
2018-03-16 09:58:38 +0530

    Checkpoint 01.

--- app/src/main/java/com/example/musketeers/realm/DashboardActivity.java
+++ app/src/main/java/com/example/musketeers/realm/DashboardActivity.java
@@ -50,23 +50,14 @@ public class DashboardActivity extends AppCompatActivity {
 
         speak = findViewById(R.id.speak);
 
-        if (eco.isChecked()) {
-            eHeater.setVisibility(View.VISIBLE);
-            eIron.setVisibility(View.VISIBLE);
-            eOutLight.setVisibility(View.VISIBLE);
-            eBedLight.setVisibility(View.VISIBLE);
-            eMotor.setVisibility(View.VISIBLE);
-            eBedFan.setVisibility(View.VISIBLE);
-            eWash.setVisibility(View.VISIBLE);
-        } else {
-            eHeater.setVisibility(View.INVISIBLE);
-            eIron.setVisibility(View.INVISIBLE);
-            eOutLight.setVisibility(View.INVISIBLE);
-            eBedLight.setVisibility(View.INVISIBLE);
-            eMotor.setVisibility(View.INVISIBLE);
-            eBedFan.setVisibility(View.INVISIBLE);
-            eWash.setVisibility(View.INVISIBLE);
-        }
+        ecoMode(eco);
+
+        eco.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                reply(eco);
+            }
+        });
 
         tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
             @Override
@@ -80,22 +71,117 @@ public class DashboardActivity extends AppCompatActivity {
 
     public void ecoMode(View view) {
         if (eco.isChecked()) {
-            eHeater.setVisibility(View.VISIBLE);
-            eIron.setVisibility(View.VISIBLE);
-            eOutLight.setVisibility(View.VISIBLE);
-            eBedLight.setVisibility(View.VISIBLE);
-            eMotor.setVisibility(View.VISIBLE);
-            eBedFan.setVisibility(View.VISIBLE);
-            eWash.setVisibility(View.VISIBLE);
+            eHeater.setChecked(true);
+            eIron.setChecked(true);
+            eOutLight.setChecked(true);
+            eBedLight.setChecked(true);
+            eMotor.setChecked(true);
+            eBedFan.setChecked(true);
+            eWash.setChecked(true);
         } else {
-            eHeater.setVisibility(View.INVISIBLE);
-            eIron.setVisibility(View.INVISIBLE);
-            eOutLight.setVisibility(View.INVISIBLE);
-            eBedLight.setVisibility(View.INVISIBLE);
-            eMotor.setVisibility(View.INVISIBLE);
-            eBedFan.setVisibility(View.INVISIBLE);
-            eWash.setVisibility(View.INVISIBLE);
+            eHeater.setChecked(false);
+            eIron.setChecked(false);
+            eOutLight.setChecked(false);
+            eBedLight.setChecked(false);
+            eMotor.setChecked(false);
+            eBedFan.setChecked(false);
+            eWash.setChecked(false);
         }
+    }
+
+    public void reply(View view) {
+        if (view.getId() == R.id.heater) {
+            if (heater.isChecked()) {
+                command = "turn on water heater";
+            } else {
+                command = "turn off water heater";
+            }
+        } else if (view.getId() == R.id.eheater) {
+            if (eHeater.isChecked()) {
+                command = "turn on eco water heater";
+            } else {
+                command = "turn off eco water heater";
+            }
+        } else if (view.getId() == R.id.iron) {
+            if (iron.isChecked()) {
+                command = "turn on iron box";
+            } else {
+                command = "turn off iron box";
+            }
+        } else if (view.getId() == R.id.eiron) {
+            if (eIron.isChecked()) {
+                command = "turn on eco iron box";
+            } else {
+                command = "turn off eco iron box";
+            }
+        } else if (view.getId() == R.id.outlight) {
+            if (outLight.isChecked()) {
+                command = "turn on outside light";
+            } else {
+                command = "turn off outside light";
+            }
+        } else if (view.getId() == R.id.eoutlight) {
+            if (eOutLight.isChecked()) {
+                command = "turn on eco outside light";
+            } else {
+                command = "turn off eco outside light";
+            }
+        } else if (view.getId() == R.id.bedlight) {
+            if (bedLight.isChecked()) {
+                command = "turn on bedroom light";
+            } else {
+                command = "turn off bedroom light";
+            }
+        } else if (view.getId() == R.id.ebedlight) {
+            if (eBedLight.isChecked()) {
+                command = "turn on eco bedroom light";
+            } else {
+                command = "turn off eco bedroom light";
+            }
+        } else if (view.getId() == R.id.motor) {
+            if (motor.isChecked()) {
+                command = "turn on water motor";
+            } else {
+                command = "turn off water motor";
+            }
+        } else if (view.getId() == R.id.emotor) {
+            if (eMotor.isChecked()) {
+                command = "turn on eco water motor";
+            } else {
+                command = "turn off eco water motor";
+            }
+        } else if (view.getId() == R.id.bedfan) {
+            if (bedFan.isChecked()) {
+                command = "turn on bedroom fan";
+            } else {
+                command = "turn off bedroom fan";
+            }
+        } else if (view.getId() == R.id.ebedfan) {
+            if (eBedFan.isChecked()) {
+                command = "turn on eco bedroom fan";
+            } else {
+                command = "turn off eco bedroom fan";
+            }
+        } else if (view.getId() == R.id.wash) {
+            if (wash.isChecked()) {
+                command = "turn on washing machine";
+            } else {
+                command = "turn off washing machine";
+            }
+        } else if (view.getId() == R.id.ewash) {
+            if (eWash.isChecked()) {
+                command = "turn on eco washing machine";
+            } else {
+                command = "turn off eco washing machine";
+            }
+        } else if (view.getId() == R.id.eco) {
+            if (eco.isChecked()) {
+                command = "eco mode on";
+            } else {
+                command = "eco mode off";
+            }
+        }
+        makeToast(command);
     }
 
     public void talk(View view) {
@@ -147,155 +233,146 @@ public class DashboardActivity extends AppCompatActivity {
                 eco.setChecked(false);
                 ecoMode(eco);
                 reply = "eco mode turned off";
-            } else if (eco.isChecked()) {
-                if (cmd.contains("water heater")) {
-                    if (cmd.contains("on")) {
-                        eHeater.setChecked(true);
-                        reply = "eco mode of water heater turned on";
-                    } else if (cmd.contains("off")) {
-                        eHeater.setChecked(false);
-                        reply = "eco mode of water heater turned off";
-                    } else {
-                        reply = "Pardon! Speak Again.";
-                    }
-                } else if (cmd.contains("iron box")) {
-                    if (cmd.contains("on")) {
-                        eIron.setChecked(true);
-                        reply = "eco mode of iron box turned on";
-                    } else if (cmd.contains("off")) {
-                        eIron.setChecked(false);
-                        reply = "eco mode of iron box turned off";
-                    } else {
-                        reply = "Pardon! Speak Again.";
-                    }
-                } else if (cmd.contains("outside light")) {
-                    if (cmd.contains("on")) {
-                        eOutLight.setChecked(true);
-                        reply = "eco mode of outside light turned on";
-                    } else if (cmd.contains("off")) {
-                        eOutLight.setChecked(false);
-                        reply = "eco mode of outside light turned off";
-                    } else {
-                        reply = "Pardon! Speak Again.";
-                    }
-                } else if (cmd.contains("bedroom light")) {
-                    if (cmd.contains("on")) {
-                        eBedLight.setChecked(true);
-                        reply = "eco mode of bedroom light turned on";
-                    } else if (cmd.contains("off")) {
-                        eBedLight.setChecked(false);
-                        reply = "eco mode of bedroom light turned off";
-                    } else {
-                        reply = "Pardon! Speak Again.";
-                    }
-                } else if (cmd.contains("water motor")) {
-                    if (cmd.contains("on")) {
-                        eMotor.setChecked(true);
-                        reply = "eco mode of water motor turned on";
-                    } else if (cmd.contains("off")) {
-                        eMotor.setChecked(false);
-                        reply = "eco mode of water motor turned off";
-                    } else {
-                        reply = "Pardon! Speak Again.";
-                    }
-                } else if (cmd.contains("bedroom fan")) {
-                    if (cmd.contains("on")) {
-                        eBedFan.setChecked(true);
-                        reply = "eco mode of bedroom fan turned on";
-                    } else if (cmd.contains("off")) {
-                        eBedFan.setChecked(false);
-                        reply = "eco mode of bedroom fan turned off";
-                    } else {
-                        reply = "Pardon! Speak Again.";
-                    }
-                } else if (cmd.contains("washing machine")) {
-                    if (cmd.contains("on")) {
-                        eWash.setChecked(true);
-                        reply = "eco mode of washing machine turned on";
-                    } else if (cmd.contains("off")) {
-                        eWash.setChecked(false);
-                        reply = "eco mode of washing machine turned off";
-                    } else {
-                        reply = "Pardon! Speak Again.";
-                    }
-                } else {
-                    reply = "Pardon! Speak Again.";
-                }
-            }
-            else {
-                reply = "Pardon! Speak Again.";
-            }
-        } else if (!eco.isChecked() || eco.isChecked()) {
-            if (cmd.contains("water heater")) {
+            } else if (cmd.contains("water heater")) {
                 if (cmd.contains("on")) {
-                    heater.setChecked(true);
-                    reply = "water heater turned on";
+                    eHeater.setChecked(true);
+                    reply = "eco mode of water heater turned on";
                 } else if (cmd.contains("off")) {
-                    heater.setChecked(false);
-                    reply = "water heater turned off";
+                    eHeater.setChecked(false);
+                    reply = "eco mode of water heater turned off";
                 } else {
                     reply = "Pardon! Speak Again.";
                 }
             } else if (cmd.contains("iron box")) {
-                if (cmd.contains("on")) {
-                    iron.setChecked(true);
-                    reply = "iron box turned on";
+                if (cmd.contains("turn on")) {
+                    eIron.setChecked(true);
+                    reply = "eco mode of iron box turned on";
                 } else if (cmd.contains("off")) {
-                    iron.setChecked(false);
-                    reply = "iron box turned off";
+                    eIron.setChecked(false);
+                    reply = "eco mode of iron box turned off";
                 } else {
                     reply = "Pardon! Speak Again.";
                 }
             } else if (cmd.contains("outside light")) {
                 if (cmd.contains("on")) {
-                    outLight.setChecked(true);
-                    reply = "outside light turned on";
+                    eOutLight.setChecked(true);
+                    reply = "eco mode of outside light turned on";
                 } else if (cmd.contains("off")) {
-                    outLight.setChecked(false);
-                    reply = "outside light turned off";
+                    eOutLight.setChecked(false);
+                    reply = "eco mode of outside light turned off";
                 } else {
                     reply = "Pardon! Speak Again.";
                 }
             } else if (cmd.contains("bedroom light")) {
                 if (cmd.contains("on")) {
-                    bedLight.setChecked(true);
-                    reply = "bedroom light turned on";
+                    eBedLight.setChecked(true);
+                    reply = "eco mode of bedroom light turned on";
                 } else if (cmd.contains("off")) {
-                    bedLight.setChecked(false);
-                    reply = "bedroom light turned off";
+                    eBedLight.setChecked(false);
+                    reply = "eco mode of bedroom light turned off";
                 } else {
                     reply = "Pardon! Speak Again.";
                 }
             } else if (cmd.contains("water motor")) {
                 if (cmd.contains("on")) {
-                    motor.setChecked(true);
-                    reply = "water motor turned on";
+                    eMotor.setChecked(true);
+                    reply = "eco mode of water motor turned on";
                 } else if (cmd.contains("off")) {
-                    motor.setChecked(false);
-                    reply = "water motor turned off";
+                    eMotor.setChecked(false);
+                    reply = "eco mode of water motor turned off";
                 } else {
                     reply = "Pardon! Speak Again.";
                 }
             } else if (cmd.contains("bedroom fan")) {
                 if (cmd.contains("on")) {
-                    bedFan.setChecked(true);
-                    reply = "bedroom fan turned on";
+                    eBedFan.setChecked(true);
+                    reply = "eco mode of bedroom fan turned on";
                 } else if (cmd.contains("off")) {
-                    bedFan.setChecked(false);
-                    reply = "bedroom fan turned off";
+                    eBedFan.setChecked(false);
+                    reply = "eco mode of bedroom fan turned off";
                 } else {
                     reply = "Pardon! Speak Again.";
                 }
             } else if (cmd.contains("washing machine")) {
                 if (cmd.contains("on")) {
-                    wash.setChecked(true);
-                    reply = "washing machine turned on";
+                    eWash.setChecked(true);
+                    reply = "eco mode of washing machine turned on";
                 } else if (cmd.contains("off")) {
-                    wash.setChecked(false);
-                    reply = "washing machine turned off";
+                    eWash.setChecked(false);
+                    reply = "eco mode of washing machine turned off";
                 } else {
                     reply = "Pardon! Speak Again.";
                 }
+            } else {
+                reply = "Pardon! Speak Again.";
+            }
+        } else if (cmd.contains("water heater")) {
+            if (cmd.contains("on")) {
+                heater.setChecked(true);
+                reply = "water heater turned on";
+            } else if (cmd.contains("off")) {
+                heater.setChecked(false);
+                reply = "water heater turned off";
+            } else {
+                reply = "Pardon! Speak Again.";
+            }
+        } else if (cmd.contains("iron box")) {
+            if (cmd.contains("turn on")) {
+                iron.setChecked(true);
+                reply = "iron box turned on";
+            } else if (cmd.contains("off")) {
+                iron.setChecked(false);
+                reply = "iron box turned off";
+            } else {
+                reply = "Pardon! Speak Again.";
+            }
+        } else if (cmd.contains("outside light")) {
+            if (cmd.contains("on")) {
+                outLight.setChecked(true);
+                reply = "outside light turned on";
+            } else if (cmd.contains("off")) {
+                outLight.setChecked(false);
+                reply = "outside light turned off";
+            } else {
+                reply = "Pardon! Speak Again.";
+            }
+        } else if (cmd.contains("bedroom light")) {
+            if (cmd.contains("on")) {
+                bedLight.setChecked(true);
+                reply = "bedroom light turned on";
+            } else if (cmd.contains("off")) {
+                bedLight.setChecked(false);
+                reply = "bedroom light turned off";
+            } else {
+                reply = "Pardon! Speak Again.";
+            }
+        } else if (cmd.contains("water motor")) {
+            if (cmd.contains("on")) {
+                motor.setChecked(true);
+                reply = "water motor turned on";
+            } else if (cmd.contains("off")) {
+                motor.setChecked(false);
+                reply = "water motor turned off";
+            } else {
+                reply = "Pardon! Speak Again.";
+            }
+        } else if (cmd.contains("bedroom fan")) {
+            if (cmd.contains("on")) {
+                bedFan.setChecked(true);
+                reply = "bedroom fan turned on";
+            } else if (cmd.contains("off")) {
+                bedFan.setChecked(false);
+                reply = "bedroom fan turned off";
+            } else {
+                reply = "Pardon! Speak Again.";
+            }
+        } else if (cmd.contains("washing machine")) {
+            if (cmd.contains("on")) {
+                wash.setChecked(true);
+                reply = "washing machine turned on";
+            } else if (cmd.contains("off")) {
+                wash.setChecked(false);
+                reply = "washing machine turned off";
             } else {
                 reply = "Pardon! Speak Again.";
             }
