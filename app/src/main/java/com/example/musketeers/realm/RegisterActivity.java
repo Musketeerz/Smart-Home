675ac356f9611a7e582076139e142206178f2596
RajPrabhakar <rajprabhakar1804@gmail.com>
2018-03-19 17:16:34 +0530

    Voice Registration done.

--- app/src/main/java/com/example/musketeers/realm/RegisterActivity.java
+++ app/src/main/java/com/example/musketeers/realm/RegisterActivity.java
@@ -1,27 +1,42 @@
 package com.example.musketeers.realm;
 
 import android.Manifest;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.test.mock.MockPackageManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Locale;
+
 public class RegisterActivity extends AppCompatActivity {
 
+    private static final String TAG = "Voice Register";
     EditText et1, et2, et4;
     LinearLayout et3;
     ImageView loc;
+    FloatingActionButton fab;
     String name, aadhar, econsumer, location = null;
+    private String command, reply;
+    int field = 1;
 
     GPSTracker gps;
+    private final int REQ_CODE_SPEECH_INPUT = 100;
+    private TextToSpeech tts;
 
     private static final int REQUEST_CODE_PERMISSION = 2;
     String mPermission = Manifest.permission.ACCESS_FINE_LOCATION;
@@ -35,6 +50,7 @@ public class RegisterActivity extends AppCompatActivity {
         et2 = findViewById(R.id.aadharField);
         et3 = findViewById(R.id.locationField);
         et4 = findViewById(R.id.econsumerField);
+        fab = findViewById(R.id.speak);
 
         try {
             if (ActivityCompat.checkSelfPermission(this, mPermission) != MockPackageManager.PERMISSION_GRANTED) {
@@ -45,21 +61,27 @@ public class RegisterActivity extends AppCompatActivity {
         }
 
         loc = findViewById(R.id.gps);
-        et3.setOnClickListener(new View.OnClickListener() {
+
+        tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
             @Override
-            public void onClick(View view) {
-                gps = new GPSTracker(RegisterActivity.this);
-                if (gps.canGetLocation()) {
-                    double latitude = gps.getLatitude();
-                    double longitude = gps.getLongitude();
-                    location = latitude + "," + longitude;
-                    loc.setImageResource(R.drawable.tick1);
-                    //Toast.makeText(getApplicationContext(), "Your location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
-                } else {
-                    gps.showSettingsAlert();
+            public void onInit(int i) {
+                if (i != TextToSpeech.ERROR) {
+                    tts.setLanguage(Locale.UK);
                 }
             }
         });
+    }
+
+    public void location(View view) {
+        gps = new GPSTracker(RegisterActivity.this);
+        if (gps.canGetLocation()) {
+            double latitude = gps.getLatitude();
+            double longitude = gps.getLongitude();
+            location = latitude + "," + longitude;
+            loc.setImageResource(R.drawable.tick1);
+        } else {
+            gps.showSettingsAlert();
+        }
     }
 
     public void register(View view) {
@@ -67,7 +89,7 @@ public class RegisterActivity extends AppCompatActivity {
         aadhar = et2.getText().toString();
         econsumer = et4.getText().toString();
 
-        if ((!name.isEmpty()) && (!aadhar.isEmpty()) && (aadhar.length() > 11) && (!econsumer.isEmpty()) && (location != null)) {
+        if ((!name.isEmpty()) && (!aadhar.isEmpty()) && (aadhar.length() > 11) && (!econsumer.isEmpty()) && (econsumer.length() > 9) && (location != null)) {
             Intent i = new Intent(this, PairActivity.class);
             i.putExtra(PairActivity.aadhar_name, aadhar);
             i.putExtra(PairActivity.econsumer_name, econsumer);
@@ -75,5 +97,109 @@ public class RegisterActivity extends AppCompatActivity {
         } else {
             Toast.makeText(getApplicationContext(), "Enter Valid Credentials", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    public void talk(View view) {
+        switch (field) {
+            case 1:
+                tts.speak("Please tell your name", TextToSpeech.QUEUE_FLUSH, null);
+                break;
+            case 2:
+                tts.speak("Please tell your 12 digit aadhar number", TextToSpeech.QUEUE_FLUSH, null);
+                break;
+            case 3:
+                tts.speak("Please tell your 10 digit electricity consumer number", TextToSpeech.QUEUE_FLUSH, null);
+                break;
+        }
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for(int progress = 0; progress<100; progress++) {
+                    try {
+                        Thread.sleep(25);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+                promptSpeechInput();
+            }
+        }).start();
+    }
+
+    private void promptSpeechInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                getString(R.string.speech_prompt));
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(),
+                    getString(R.string.speech_not_supported),
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    command = result.get(0).toLowerCase();
+                    makeToast(command);
+                }
+                break;
+            }
+
+        }
+    }
+
+    private void makeToast(String cmd) {
+        switch (field) {
+            case 1:
+                et1.setText(cmd);
+                break;
+            case 2:
+                if (cmd.length() > 11) {
+                    cmd = cmd.replace(" ", "");
+                    et2.setText(cmd);
+                }
+                else
+                    talk(fab);
+                break;
+            case 3:
+                if (cmd.length() > 9) {
+                    cmd = cmd.replace(" ","");
+                    et4.setText(cmd);
+                }
+                else
+                    talk(fab);
+                location(et3);
+                break;
+        }
+        Toast.makeText(getApplicationContext(), cmd, Toast.LENGTH_SHORT).show();
+        tts.speak(cmd, TextToSpeech.QUEUE_FLUSH, null);
+        field ++;
+        if (field < 4)
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for(int progress = 0; progress<100; progress++) {
+                        try {
+                            Thread.sleep(40);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    talk(fab);
+                }
+            }).start();
     }
 }
